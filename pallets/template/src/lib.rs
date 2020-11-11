4ff56284de7a9467beae71b4e8bbd4fbb595a4dc
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-11-11 20:34:38 +0100

    Added storage item for a custom person struct and Dispatchable function for adding a person to storage

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,14 +4,21 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Vec};
 use frame_system::ensure_signed;
+use frame_support::codec::{Encode, Decode};
 
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
+
+#[derive(Encode, Decode, Debug, Default, Clone, PartialEq, Eq)]
+pub struct Person {
+    name: Vec<u8>,
+    age: u8,
+}
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -25,10 +32,12 @@ decl_storage! {
 	// A unique name is used to ensure that the pallet's storage items are isolated.
 	// This name may be updated, but each pallet in the runtime must use a unique name.
 	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
+	trait Store for Module<T: Trait> as Persons {
 		// Learn more about declaring storage items:
 		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		//Something get(fn something): Option<u32>;
+		Persons get(fn get_persons):
+			map hasher(blake2_128_concat) T::AccountId => Person;
 	}
 }
 
@@ -38,7 +47,8 @@ decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
+		//SomethingStored(u32, AccountId),
+		PersonAdded(Person, AccountId),
 	}
 );
 
@@ -63,41 +73,20 @@ decl_module! {
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+		#[weight = 10000]
+		fn add_person(origin, name: Vec<u8>, age: u8) -> dispatch::DispatchResult {
 			let who = ensure_signed(origin)?;
 
-			// Update storage.
-			Something::put(something);
+			let new_person = Person{
+				name,
+				age,
+			};
 
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
+			<Persons<T>>::insert(&who, new_person.clone());
+
+			Self::deposit_event(RawEvent::PersonAdded(new_person, who));
+			
 			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
 		}
 	}
 }
